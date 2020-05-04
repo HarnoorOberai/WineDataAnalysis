16fee7ff09f43cb3c5528d6d96e1ace1f1e598d9
yashwin97 <36672725+yashwin97@users.noreply.github.com>
2020-05-04 19:23:33 +0100

    Add files via upload

---
+++ Random-Forest/Random-Red.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import export_graphviz
+import matplotlib.pyplot as plt
+import pydot
+
+features=pd.read_csv("winequality-red.csv",sep=";")
+labels = np.array(features['quality'])
+features=features.drop('quality',axis=1)
+feature_list=list(features.columns)
+features = np.array(features)
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+print('Training Features Shape:', train_features.shape)
+print('Training Labels Shape:', train_labels.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Labels Shape:', test_labels.shape)
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+# Train the model on training data
+rf.fit(train_features, train_labels)
+# Use the forest's predict method on the test data
+predictions = rf.predict(test_features)
+# Calculate the absolute errors
+errors = abs(predictions - test_labels)
+# Print out the mean absolute error (mae)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+# Calculate mean absolute percentage error (MAPE)
+mape = 100 * (errors / test_labels)
+# Calculate and display accuracy
+accuracy = 100 - np.mean(mape)
+print('Accuracy:', round(accuracy, 2), '%.')
+# Pull out one tree from the forest
+tree = rf.estimators_[5]
+# Export the image to a dot file
+export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 1)
+# Use dot file to create a graph
+(graph, ) = pydot.graph_from_dot_file('tree.dot')
+# Write graph to a png file
+graph.write_png('tree.png')
+# Limit depth of tree to 3 levels
+rf_small = RandomForestRegressor(n_estimators=10, max_depth = 4)
+rf_small.fit(train_features, train_labels)
+# Extract the small tree
+tree_small = rf_small.estimators_[7]
+# Save the tree as a png image
+export_graphviz(tree_small, out_file = 'small_tree.dot', feature_names = feature_list, rounded = True, precision = 1)
+(graph, ) = pydot.graph_from_dot_file('small_tree.dot')
+graph.write_png('small_tree.png');
+# Get numerical feature importances
+importances = list(rf.feature_importances_)
+# List of tuples with variable and importance
+feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+# Sort the feature importances by most important first
+feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+# Print out the feature and importances
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+# New random forest with only the two most important variables
+rf_most_important = RandomForestRegressor(n_estimators= 1000, random_state=42)
+# Extract the two most important features
+important_indices = [feature_list.index('alcohol'), feature_list.index('sulphates')]
+train_important = train_features[:, important_indices]
+test_important = test_features[:, important_indices]
+# Train the random forest
+rf_most_important.fit(train_important, train_labels)
+# Make predictions and determine the error
+predictions = rf_most_important.predict(test_important)
+errors = abs(predictions - test_labels)
+# Display the performance metrics
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+mape = np.mean(100 * (errors / test_labels))
+accuracy = 100 - mape
+print('Accuracy using Important Variables:', round(accuracy, 2), '%.')
+# # Set the style
+# plt.style.use('fivethirtyeight')
+# # list of x locations for plotting
+# x_values = list(range(len(importances)))
+# # Make a bar chart
+# plt.bar(x_values, importances, orientation = 'vertical')
+# # Tick labels for x axis
+# plt.xticks(x_values, feature_list, rotation='vertical')
+# # Axis labels and title
+# plt.ylabel('Importance'); plt.xlabel('Variable'); plt.title('Variable Importances');
+# plt.show()
+
+plt.plot(test_labels, 'b-', label = 'actual')
+# Plot the predicted values
+plt.plot(predictions, 'ro', label = 'prediction')
+plt.xticks(rotation = '60');
+plt.legend()
+# Graph labels
+plt.xlabel('Sample'); plt.ylabel('Quality'); plt.title('Actual and Predicted Values');
+plt.show()
