e5f0f229f3d3437f1467db1173c1abd93d47f28b
Harnoor <harnoorqmul@gmail.com>
2020-05-11 12:23:31 +0100

    SVC model created

--- Random-Forest/Random-Red.py
+++ Random-Forest/Random-Red.py
@@ -1,3 +1,4 @@
+# import pair as pair
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -6,18 +7,19 @@ from sklearn.tree import export_graphviz
 import matplotlib.pyplot as plt
 import pydot
 
-features=pd.read_csv("winequality-red.csv",sep=";")
+features = pd.read_csv("winequality-red.csv", sep=";")
 labels = np.array(features['quality'])
-features=features.drop('quality',axis=1)
-feature_list=list(features.columns)
+features = features.drop('quality', axis=1)
+feature_list = list(features.columns)
 features = np.array(features)
-train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.25,
+                                                                            random_state=42)
 print('Training Features Shape:', train_features.shape)
 print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
 # Instantiate model with 1000 decision trees
-rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+rf = RandomForestRegressor(n_estimators=1000, random_state=42)
 # Train the model on training data
 rf.fit(train_features, train_labels)
 # Use the forest's predict method on the test data
@@ -34,30 +36,30 @@ print('Accuracy:', round(accuracy, 2), '%.')
 # Pull out one tree from the forest
 tree = rf.estimators_[5]
 # Export the image to a dot file
-export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-# Use dot file to create a graph
-(graph, ) = pydot.graph_from_dot_file('tree.dot')
+export_graphviz(tree, out_file='tree.dot', feature_names=feature_list, rounded=True, precision=1)
+# # Use dot file to create a graph
+(graph,) = pydot.graph_from_dot_file('tree.dot')
 # Write graph to a png file
-graph.write_png('tree.png')
+# graph.write_png('tree.png')
 # Limit depth of tree to 3 levels
-rf_small = RandomForestRegressor(n_estimators=10, max_depth = 4)
+rf_small = RandomForestRegressor(n_estimators=10, max_depth=4)
 rf_small.fit(train_features, train_labels)
 # Extract the small tree
 tree_small = rf_small.estimators_[7]
 # Save the tree as a png image
-export_graphviz(tree_small, out_file = 'small_tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-(graph, ) = pydot.graph_from_dot_file('small_tree.dot')
-graph.write_png('small_tree.png');
+export_graphviz(tree_small, out_file='small_tree.dot', feature_names=feature_list, rounded=True, precision=1)
+(graph,) = pydot.graph_from_dot_file('small_tree.dot')
+# graph.write_png('small_tree.png')
 # Get numerical feature importances
 importances = list(rf.feature_importances_)
 # List of tuples with variable and importance
 feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
 # Sort the feature importances by most important first
-feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
 # Print out the feature and importances
-[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+# print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances
 # New random forest with only the two most important variables
-rf_most_important = RandomForestRegressor(n_estimators= 1000, random_state=42)
+rf_most_important = RandomForestRegressor(n_estimators=1000, random_state=42)
 # Extract the two most important features
 important_indices = [feature_list.index('alcohol'), feature_list.index('sulphates')]
 train_important = train_features[:, important_indices]
@@ -84,11 +86,13 @@ print('Accuracy using Important Variables:', round(accuracy, 2), '%.')
 # plt.ylabel('Importance'); plt.xlabel('Variable'); plt.title('Variable Importances');
 # plt.show()
 
-plt.plot(test_labels, 'b-', label = 'actual')
+plt.plot(test_labels, 'b-', label='actual')
 # Plot the predicted values
-plt.plot(predictions, 'ro', label = 'prediction')
-plt.xticks(rotation = '60');
+plt.plot(predictions, 'ro', label='prediction')
+plt.xticks(rotation='60');
 plt.legend()
 # Graph labels
-plt.xlabel('Sample'); plt.ylabel('Quality'); plt.title('Actual and Predicted Values');
+plt.xlabel('Sample');
+plt.ylabel('Quality');
+plt.title('Actual and Predicted Values');
 plt.show()
